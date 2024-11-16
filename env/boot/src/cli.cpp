e2d8f83acba9f8b7ae6213d6338910936b5dc474
Pavlenko Eugene <pavlenko.obs@gmail.com>
2024-11-16 19:51:19 +0200

    find a way to init commands

--- env/boot/src/cli.cpp
+++ env/boot/src/cli.cpp
@@ -7,7 +7,16 @@ void CLI_Writer(const char *str)
     STM32::UART1_Driver::send((uint8_t *)str, strlen(str), nullptr);
 }
 
+//define handlers...
+void CLI_HelpCommand(){}
+
 void CLI_Init()
 {
-    //TODO add commands
+    //TODO somehow add commands, maybe use prev/next refs for dynamic allocation...
+    // Command{Command *prev, Command *next}...
+    // const V2::Command Help{"?"};
+
+    const V2::Command commands[] = {V2::Command{"?", CLI_HelpCommand}};
+
+    CLI::configure(commands);
 }
--- lib/console/inc/console.hpp
+++ lib/console/inc/console.hpp
@@ -3,8 +3,10 @@
 
 #include <algorithm>
 #include <array>
+#include <cerrno>
 #include <cstdint>
-#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
 
 // struct for allow pass string as template parameter & use at compile time
 // template <std::size_t N>
@@ -59,10 +61,12 @@ namespace V2
         void *any_value;
     };
 
-    // Command
+    typedef void (*CommandHandler)(void);
+
     struct Command
     {
         const char *name;
+        const CommandHandler handler;
     };
 
     typedef void (*ConsoleWriter)(const char *);
@@ -71,27 +75,72 @@ namespace V2
     class Console
     {
         static constexpr const ConsoleWriter writer = tWriter;
-        static constexpr const std::array<Command, sizeof...(tCommands)> commands{tCommands()...}; //<--???
+        static constexpr const std::array<Command*, sizeof...(tCommands)> commands{tCommands()...}; //<--???
+        //TODO need init somehow in other way
 
-        static inline bool parseArg(const char *buf, ArgType type, ArgValue *val)
+        static inline Command *find(const char *str)
+        {
+            for (Command *cmd : commands)
+            {
+                if (0 == strcmp(cmd->name, str))
+                    return cmd;
+            }
+            return nullptr;
+        }
+
+        static inline bool parseArg(const char *str, ArgType type, ArgValue *val)
         {
             switch (type)
             {
             case ArgType::INT:
-                int32_t result = atol(buf);
+                int32_t result = atol(str);
+                if (errno)
+                    return false;
                 val->int_value = result;
                 return true;
             case ArgType::STR:
-                val->str_value = buf;
+                val->str_value = str;
                 return true;
             }
             return false;
         }
 
     public:
+        static inline void configure(const Command commands[])
+        {
+            //static Command *commands
+            //static size_t commands_len
+        }
+
         static inline void process(char *buf, size_t len)
         {
-            //
+            const Command *cmd = nullptr;
+            const char *token = nullptr;
+
+            for (size_t i = 0; i < len; i++)
+            {
+                const char c = buf[i];
+                if (c == ' ' || c == '\0')
+                {
+                    if (!token)
+                        return; // Empty line or extra whitespace - just skip handle
+
+                    buf[i] = '\0'; // Replace char to end terminator for proper parse arg
+                }
+                if (!cmd)
+                {
+                    cmd = find(token);
+                    if (!cmd)
+                    {
+                        write("ERR: command not found");
+                        return;
+                    }
+                }
+                else if (!token)
+                {
+                    token = &buf[i]; // Set token addr if not set
+                }
+            }
         }
         static inline void write(const char *str)
         {
