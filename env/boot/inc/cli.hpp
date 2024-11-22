e47660ab0f5a761d1639d154068accf7f4424583
Pavlenko Eugene <pavlenko.obs@gmail.com>
2024-11-22 18:51:31 +0200

    add console configurable buffers & wrap into namespace for prevent collisions

--- env/boot/inc/cli.hpp
+++ env/boot/inc/cli.hpp
@@ -3,6 +3,21 @@
 
 #include <Console.hpp>
 
+namespace Console
+{
+#if defined(CONSOLE_STDIN_BUFFER_SIZE)
+    static const size_t STDIN_BUFFER_SIZE = CONSOLE_STDIN_BUFFER_SIZE;
+#else
+    static const size_t STDIN_BUFFER_SIZE = CONSOLE_STDIN_BUFFER_SIZE;
+#endif
+
+#if defined(CONSOLE_STDOUT_BUFFER_SIZE)
+    static const size_t STDOUT_BUFFER_SIZE = CONSOLE_STDOUT_BUFFER_SIZE;
+#else
+    static const size_t STDOUT_BUFFER_SIZE = CONSOLE_STDOUT_BUFFER_SIZE;
+#endif
+}
+
 void CLI_Writer(const char *str);
 
 using CLI = V2::Console<CLI_Writer>;
--- env/boot/src/cli.cpp
+++ env/boot/src/cli.cpp
@@ -4,8 +4,8 @@
 
 void CLI_Writer(const char *str)
 {
-    //TODO make buffered output, if received \n char then we can flush buffer
-    //TODO on flush - copy data to tx buffer, clear cli buffer, then send tx buffer...
+    // TODO make buffered output, if received \n char then we can flush buffer
+    // TODO on flush - copy data to tx buffer, clear cli buffer, then send tx buffer...
     STM32::UART1_Driver::send((uint8_t *)str, strlen(str), nullptr);
 }
 
@@ -17,8 +17,8 @@ void CLI_HelpCommand()
 
 void CLI_Init()
 {
-    //TODO in/out buffers, also allow read/write them outside of console
-    //TODO move commands array to global namespace or annonymous for allow access from help command
+    // TODO in/out buffers, also allow read/write them outside of console
+    // TODO move commands array to global namespace or annonymous for allow access from help command
     static V2::Command commands[] = {
         V2::Command{"?", CLI_HelpCommand}
     };
@@ -26,14 +26,16 @@ void CLI_Init()
     CLI::configure(commands, sizeof(commands) / sizeof(V2::Command));
 }
 
-//TODO names & configurable params...
-#define MAX 256
-static char stdin[MAX];
-static char stdout[MAX];
-//TODO commands definitions here
+namespace Console
+{
+    static uint8_t STDIN[STDIN_BUFFER_SIZE];
+    static uint8_t STDOUT[STDOUT_BUFFER_SIZE];
+    // TODO commands definitions here
+}
 
 Console::App &ConsoleApp()
 {
-    static Console::App console{nullptr, 0, stdin, sizeof(stdin), stdout, sizeof(stdout)};
+    using namespace Console;
+    static Console::App console{nullptr, 0, STDIN, STDIN_BUFFER_SIZE, STDOUT, STDOUT_BUFFER_SIZE};
     return console;
 }
