590f2093acf32c917ceca9227cbf3baaa7cfd9d1
Павленко Евгений <pavlenko.obs@gmail.com>
2024-12-26 15:08:55 +0200

    Update register.hpp

--- lib/stm32/inc/stm32/common/register.hpp
+++ lib/stm32/inc/stm32/common/register.hpp
@@ -2,6 +2,13 @@
 
 namespace STM32::Register
 {
-    //todo constexpr get full address of register by base and offset of struct field
-    static inline size_t address_of();
+    /**
+     * @brief Calculate register address by CMSIS struct
+     */
+    template <uint32_t tBaseAddress, typename T1, typename T2>
+    static constexpr size_t addressOf(T1 T2::*member)
+    {
+        constexpr T2 obj {};
+        return tBaseAddress + size_t(&(obj.*member)) - size_t(&obj);
+    }
 }
