0605cce3f3a0611eb058ab2d455e317d6b52b540
Павленко Евгений <pavlenko.obs@gmail.com>
2025-01-05 13:55:47 +0200

    Update dma_definitions.hpp

--- lib/stm32/inc/stm32/common/dma_definitions.hpp
+++ lib/stm32/inc/stm32/common/dma_definitions.hpp
@@ -8,12 +8,22 @@ namespace STM32::DMA
 
     /**
      * @brief DMA channel APIs
+     *
+     * @tparam tDriver      Bus driver
+     * @tparam tRegsAddress Base address of channel registers
+     * @tparam tChannel     Channel number
+     * @tparam tIRQn        Channel IRQ number
      */
     template <typename tDriver, uint32_t tRegsAddress, uint32_t tChannel, IRQn_Type tIRQn>
     class Channel 
     {
         /**
          * @brief Transfer data via DMA
+         *
+         * @param config Transfer configuration
+         * @param buffer Data buffer ptr
+         * @param periph Peripheral buffer address
+         * @param size   Transfer size
          */
         static inline void transfer(Config config, const void* buffer, volatile void* periph, uint32_t size);
 
@@ -25,8 +35,47 @@ namespace STM32::DMA
 
     /**
      * @brief DMA bus APIs
+     *
+     * @tparam tRegsAddress Base address of bus registers
+     * @tparam tClock       Bus clock control
      */
     template <uint32_t tRegsAddress, typename tClock>
     class Driver
-    {};
+    {
+        /**
+         * @brief Enable DMA clock
+         */
+        static inline void enable();
+
+        /**
+         * @brief Disable DMA clock
+         */
+        static inline void disable();
+
+        /**
+         * @brief Check channel flag(s) is set
+         *
+         * @tparam tChannel Channel num
+         * @tparam tFlag    Flag(s) to check
+         */
+        template <uint8_t tChannel, Flag tFlag>
+        static inline bool hasChannelFlag();
+
+        /**
+         * @brief Clear channel flag(s)
+         *
+         * @tparam tChannel Channel num
+         * @tparam tFlag    Flag(s) to clear
+         */
+        template <uint8_t tChannel, Flag tFlag>
+        static inline void clrChannelFlag();
+
+        /**
+         * @brief Clear channel all flags
+         *
+         * @tparam tChannel Channel num
+         */
+        template <uint8_t tChannel>
+        static inline void clrChannelFlags();
+    };
 }
