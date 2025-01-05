b34b2634f78106dd873335cbc1e8aaa25c2d973d
Павленко Евгений <pavlenko.obs@gmail.com>
2025-01-05 14:31:48 +0200

    Update dma_definitions.hpp

--- lib/stm32/inc/stm32/common/dma_definitions.hpp
+++ lib/stm32/inc/stm32/common/dma_definitions.hpp
@@ -2,7 +2,66 @@
 
 namespace STM32::DMA
 {
-    enum class Config;
+    enum class Config
+    {
+#ifdef DMA_CCR_EN
+        // Direction
+        PER_2_MEM = 0x00000000u,
+        MEM_2_PER = DMA_CCR_DIR,
+        MEM_2_MEM = DMA_CCR_MEM2MEM,
+        // Circular mode
+        CIRCULAR = DMA_CCR_CIRC,
+        // Increments
+        PINC = DMA_CCR_PINC,
+        MINC = DMA_CCR_MINC,
+        // Periph data size
+        PSIZE_8BIT = 0x00000000u,
+        PSIZE_16BIT = DMA_CCR_PSIZE_0,
+        PSIZE_32BIT = DMA_CCR_PSIZE_1,
+        // Memory data size
+        MSIZE_8BIT = 0x00000000u,
+        MSIZE_16BIT = DMA_CCR_MSIZE_0,
+        MSIZE_32BIT = DMA_CCR_MSIZE_1,
+        // Priority
+        PRIORITY_LOW = 0x00000000u,
+        PRIORITY_MEDIUM = DMA_CCR_PL_0,
+        PRIORITY_HIGH = DMA_CCR_PL_1,
+        PRIORITY_VERY_HIGH = DMA_CCR_PL,
+        // Interrupts
+        IRQ_TRANSFER_ERROR = DMA_CCR_TEIE,
+        IRQ_TRANSFER_COMPLETE = DMA_CCR_TCIE,
+        IRQ_HALF_TRANSFER = DMA_CCR_HTIE,
+#endif
+#ifdef DMA_SxCR_EN
+        // Direction
+		PER_2_MEM = 0x00000000,
+		MEM_2_PER = DMA_SxCR_DIR_0,
+		MEM_2_MEM = DMA_SxCR_DIR_1,
+        // Circular mode
+        CIRCULAR = DMA_SxCR_CIRC,
+        // Increments
+        MINC = DMA_SxCR_MINC,
+		PINC = DMA_SxCR_PINC,
+        // Periph data size
+        PSIZE_8BIT = 0x00000000,
+		PSIZE_16BIT = DMA_SxCR_PSIZE_0,
+		PSIZE_32BIT = DMA_SxCR_PSIZE_1,
+        // Memory data size
+        MSIZE_8BIT = 0x00000000,
+		MSIZE_16BIT = DMA_SxCR_MSIZE_0,
+		MSIZE_32BIT = DMA_SxCR_MSIZE_1,
+        // Priority
+        PRIORITY_LOW = 0x00000000u,
+		PRIORITY_MEDIUM = DMA_SxCR_PL_0,
+		PRIORITY_HIGH = DMA_SxCR_PL_1,
+		PRIORITY_VERY_HIGH = DMA_SxCR_PL_1 | DMA_SxCR_PL_0,
+		// Enable IRQ
+		IRQ_TRANSFER_ERROR = DMA_SxCR_TEIE,
+		IRQ_HALF_TRANSFER = DMA_SxCR_HTIE,
+		IRQ_TRANSFER_COMPLETE = DMA_SxCR_TCIE,
+        IRQ_DIRECT_MODE_ERROR = DMA_SxCR_DMEIE,
+#endif
+    };
 
     enum class Flag
     {
