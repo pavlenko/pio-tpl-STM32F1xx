547b5715240f550ff50b13975119ab1c18525c02
Павленко Евгений <pavlenko.obs@gmail.com>
2025-01-06 08:54:50 +0200

    Create flash_definitions.hpp

---
+++ lib/stm32/inc/stm32/common/flash_definitions.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+//todo
+// write by page number 
+// write buffer
+namespace STM32
+{
+    /**
+     * @brief Flash API 
+     */
+    class Flash
+    {
+    private:
+        /**
+         * @brief Wait while flash is busy
+         */
+        static inline void wait();
+
+    public:
+        /**
+         * @brief Configure flash
+         *
+         * @param uint32_t System clock frequency
+         */
+        static inline void configure(uint32_t frequency);
+
+        /**
+         * @brief Get flash total size
+         *
+         * @return Size in bytes
+         */
+        static constexpr uint32_t getSize();
+
+        /**
+         * @brief Get page size
+         *
+         * @param page Page number
+         *
+         * @return Size in bytes
+         */
+        static constexpr uint32_t getPageSize(uint16_t page);
+
+        /**
+         * @brief Get page start address
+         *
+         * @param page Page number
+         *
+         * @return Address
+         */
+        static constexpr uint32_t getPageAddress(uint16_t page);
+
+        /**
+         * @brief Lock flash for writing
+         */
+        static inline void lock();
+
+        /**
+         * @brief Unlock flash for writing
+         */
+        static inline void unlock();
+
+        /**
+         * @brief Erase single page
+         *
+         * @param page Page number
+         *
+         * @return Success state
+         */
+        static inline bool erasePage(uint16_t page);
+
+        /**
+         * @brief Write page data
+         *
+         * @param address Page address
+         * @param data Data to write
+         *
+         * @return Success state
+         */
+        static inline bool writePage(uint32_t address, uint16_t data);
+    };
+}
