4cd90e7fbabb76a6ee9fff4244bd467f4e0f33df
Павленко Евгений <pavlenko.obs@gmail.com>
2025-01-06 13:49:21 +0200

    Update io_definitions.hpp

--- lib/stm32/inc/stm32/common/io_definitions.hpp
+++ lib/stm32/inc/stm32/common/io_definitions.hpp
@@ -31,21 +31,65 @@ namespace STM32::IO
         FASTEST,
     };
 
+    /**
+     * @brief IO pin API
+     */
     template <typename tPort, uint8_t tNumber>
     class Pin
     {
     public:
+        /**
+         * @brief Configure pin mode, pull, speed...
+         */
         static inline void configure();
+
+        /**
+         * @brief Set pi AF number (if supported)
+         */
+        static inline void setAlternate(uint8_t number);
+
+        /**
+         * @brief Get pin value
+         */
+        static inline bool get();
+
+        /**
+         * @brief Set pin to 1
+         */
+        static inline void set();
+
+        /**
+         * @brief Set pin to 0
+         */
+        static inline void clr();
+
+        /**
+         * @brief Toggle pin state
+         */
+        static inline void tog();
     };
 
+    /**
+     * @brief IO port API
+     */
     template <typename tClock>
     class Port
     {
     private:
+        /**
+         * @brief Get port regs ptr
+         */
         static constexpr GPIO_TypeDef* regs();
 
     public:
+        /**
+         * @brief Enable port clock
+         */
         static inline void enable();
+
+        /**
+         * @brief Disable port clock
+         */
         static inline void disable();
     };
 }
