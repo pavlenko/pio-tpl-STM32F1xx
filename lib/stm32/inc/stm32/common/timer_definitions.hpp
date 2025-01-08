413ee770cfbba6fcd7c36acb839eb412f4a055b2
Pavlenko Eugene <pavlenko.obs@gmail.com>
2025-01-08 20:11:51 +0200

    upd timer

--- lib/stm32/inc/stm32/common/timer_definitions.hpp
+++ lib/stm32/inc/stm32/common/timer_definitions.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stm32/_cmsis.hpp>
+
 namespace STM32::Timer
 {
     // All timer`s interrupts
@@ -25,12 +27,16 @@ namespace STM32::Timer
         CC4 = TIM_DIER_CC4DE,
         TRIGGER = TIM_DIER_UDE,
     };
-    
+
     // Timer counter mode
     enum class CounterMode
     {
+        // Direction
         UP = 0x00000000U,
         DOWN = TIM_CR1_DIR,
+        // One-pulse mode
+        ONE_PULSE = TIM_CR1_OPM,
+        // Center-aligned mode
         CENTER_ALIGNED1 = TIM_CR1_CMS_0,
         CENTER_ALIGNED2 = TIM_CR1_CMS_1,
         CENTER_ALIGNED3 = TIM_CR1_CMS,
@@ -52,20 +58,20 @@ namespace STM32::Timer
     template <uint32_t tRegsAddr, IRQn_Type tIRQn, typename tClock>
     class BasicTimer
     {
-        //config: one-pulse(?)
+        template <typename tConfig>
         static inline void configure();
-static inline void setPrescaller(uint16_t);
-static inline void setAutoReload(uint16_t);
-static inline void setCounter(uint16_t);
+        static inline void setPrescaller(uint16_t prescaller);
+        static inline void setAutoReload(uint16_t autoReload);
+        static inline void setCounter(uint16_t conter);
         static inline void enable();
         static inline void disable();
         static inline void attachIRQ(IRQFlags flags);
         static inline void detachIRQ(IRQFlags flags);
         static inline void attachDMARequest();
         static inline void detachDMARequest();
-static inline bool hasIRQ();
-static inline void clrIRQ();
-static inline void start();
+        static inline bool hasIRQFlag();
+        static inline void clrIRQFlag();
+        static inline void start();
         static inline void stop();
     };
 
@@ -83,8 +89,8 @@ static inline void start();
             static inline void clrIRQFlag();
             static inline void attachIRQ();
             static inline void detachIRQ();
-            static inline void attachDMA();
-            static inline void detachDMA();
+            static inline void attachDMARequest();
+            static inline void detachDMARequest();
         };
 
     public:
@@ -99,7 +105,7 @@ static inline void start();
                 FALLING = TIM_CCER_CC1P,
                 BOTH = TIM_CCER_CC1P | TIM_CCER_CC1NP,
             };
-                
+
             // Capture mode
             enum class Mode
             {
@@ -152,7 +158,81 @@ static inline void start();
             static inline void configure();
         };
 
-        class SlaveMode;
+        class SlaveMode //TODO incomplete... need read docs
+        {
+        public:
+            // TODO use single enum for full config options
+            // Slave mode selection
+            enum class Mode : uint16_t
+            {
+                DISABLED = 0x00 << TIM_SMCR_SMS_Pos,
+                ENCODER_MODE1 = 0x01 << TIM_SMCR_SMS_Pos,
+                ENCODER_MODE2 = 0x02 << TIM_SMCR_SMS_Pos,
+                ENCODER_MODE3 = 0x03 << TIM_SMCR_SMS_Pos,
+                RESET = 0x04 << TIM_SMCR_SMS_Pos,
+                GATED = 0x05 << TIM_SMCR_SMS_Pos,
+                TRIGGER = 0x06 << TIM_SMCR_SMS_Pos,
+                EXTERNAL_CLOCK = 0x07 << TIM_SMCR_SMS_Pos,
+            };
+
+            /// Trigger selection
+            enum class Trigger : uint16_t
+            {
+                INTERNAL_TRIGGER0 = 0x00 << TIM_SMCR_TS_Pos,
+                INTERNAL_TRIGGER1 = 0x01 << TIM_SMCR_TS_Pos,
+                INTERNAL_TRIGGER2 = 0x02 << TIM_SMCR_TS_Pos,
+                INTERNAL_TRIGGER3 = 0x03 << TIM_SMCR_TS_Pos,
+                Ti1EdgeDetector = 0x04 << TIM_SMCR_TS_Pos,
+                FilteredTimerInput1 = 0x05 << TIM_SMCR_TS_Pos,
+                FilteredTimerInput2 = 0x06 << TIM_SMCR_TS_Pos,
+                ExternalTriggerInput = 0x07 << TIM_SMCR_TS_Pos,
+            };
+
+            /// External trigger filter selection
+            enum class ExternalTriggerFilter : uint16_t
+            {
+                NoFilter = 0x00 << TIM_SMCR_ETF_Pos,
+                NoDivideFilter2 = 0x01 << TIM_SMCR_ETF_Pos,
+                NoDivideFilter4 = 0x02 << TIM_SMCR_ETF_Pos,
+                NoDivideFilter8 = 0x03 << TIM_SMCR_ETF_Pos,
+                Divide2Filter6 = 0x04 << TIM_SMCR_ETF_Pos,
+                Divide2Filter8 = 0x05 << TIM_SMCR_ETF_Pos,
+                Divide4Filter6 = 0x06 << TIM_SMCR_ETF_Pos,
+                Divide4Filter8 = 0x07 << TIM_SMCR_ETF_Pos,
+                Divide8Filter6 = 0x08 << TIM_SMCR_ETF_Pos,
+                Divide8Filter8 = 0x09 << TIM_SMCR_ETF_Pos,
+                Divide16Filter5 = 0x0a << TIM_SMCR_ETF_Pos,
+                Divide16Filter6 = 0x0b << TIM_SMCR_ETF_Pos,
+                Divide16Filter8 = 0x0c << TIM_SMCR_ETF_Pos,
+                Divide32Filter5 = 0x0d << TIM_SMCR_ETF_Pos,
+                Divide32Filter6 = 0x0e << TIM_SMCR_ETF_Pos,
+                Divide32Filter8 = 0x0f << TIM_SMCR_ETF_Pos,
+            };
+
+            /// External trigger filter selection
+            enum class ExternalTriggerPrescaler : uint16_t
+            {
+                PrescalerOff = 0x00 << TIM_SMCR_ETPS_Pos,
+                Divide2 = 0x01 << TIM_SMCR_ETPS_Pos,
+                Divide4 = 0x02 << TIM_SMCR_ETPS_Pos,
+                Divide8 = 0x03 << TIM_SMCR_ETPS_Pos,
+            };
+
+            /// External clock mode 2 enable
+            enum class ExternalClockMode2 : uint16_t
+            {
+                Disabled = 0x00 << TIM_SMCR_ECE_Pos,
+                Enabled = 0x01 << TIM_SMCR_ECE_Pos,
+            };
+
+            /// External trigger polarity
+            enum class ExternalTriggerPolarity : uint16_t
+            {
+                NonInverted = 0x00 << TIM_SMCR_ETP_Pos,
+                Inverted = 0x01 << TIM_SMCR_ETP_Pos,
+            };
+
+        };
     };
 
     template <uint32_t tRegsAddr, IRQn_Type tIRQn, typename tClock>
