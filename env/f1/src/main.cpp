2b909683a5b121f48c022503adf4618f9ca20b4d
Pavlenko Eugene <pavlenko.obs@gmail.com>
2025-09-20 18:02:23 +0300

    add blink f1

--- env/f1/src/main.cpp
+++ env/f1/src/main.cpp
@@ -6,35 +6,36 @@
 #include <stm32/dev/flash.hpp>
 #include <stm32/dev/io.hpp>
 
+#include <stm32/lib/delay.hpp>
+
 int main(void)
 {
     using namespace STM32;
 
     // Clock config
     Clock::HSEClock::on();
-
     Clock::PLLClock::configure<
         Clock::PLLClock::Source::HSE,
         Clock::PLLClockConfig<9u, 1u, false> //<-- HSE 8MHz -> SYS 72 MHz
     >();
     Clock::PLLClock::on();
+
     Clock::SysClock::configure<
         Clock::SysClock::Source::PLL,
         Flash::Latency::WS2,
         Clock::SysClockConfig<Clock::AHBClock::Divider::DIV1, Clock::APB1Clock::Divider::DIV2, Clock::APB2Clock::Divider::DIV1>
     >();
-    // Clock::AHBClock::setDivider<Clock::AHBClock::Divider::DIV1>();
-    // Clock::APB1Clock::setDivider<Clock::APB1Clock::Divider::DIV2>();
-    // Clock::APB2Clock::setDivider<Clock::APB2Clock::Divider::DIV1>();
-    // Flash::configure(SystemCoreClock);
     //  Clock config end
 
     IO::PC::enable();
     IO::PC13::configure<IO::Config<IO::Mode::OUTPUT>>();
 
+    Delay::init();
+
     while (true)
     {
-        asm("nop");
+        IO::PB2::tog();
+        Delay::ms(500);
     }
     return 0;
 }
